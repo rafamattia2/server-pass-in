3b198398da1a5137b3b6fa7e46bba64c3a84f2f7
Rafael Mattia <rdmattia@inf.ufpel.edu.br>
2024-04-06 14:54:42 -0300

    🎉 Initial Commit

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import fastify from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+import { createEvent } from "./routes/create-event";
+
+const app = fastify();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(createEvent);
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("HTTP WServer Running! ");
+});
