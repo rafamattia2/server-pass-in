30e1d0828df86299a2d25c1354d27bc00376b71e
Rafael <rdmattia@inf.ufpel.edu.br>
2024-04-06 17:13:49 -0300

    🚀 add route for register attendee for event

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import {
   validatorCompiler,
 } from "fastify-type-provider-zod";
 import { createEvent } from "./routes/create-event";
+import { registerForEvent } from "./routes/register-for-event";
 
 const app = fastify();
 
@@ -11,6 +12,7 @@ app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(createEvent);
+app.register(registerForEvent);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP WServer Running! ");
