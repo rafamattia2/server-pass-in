54943ed46ba480d80f931c9c261352367f5faf62
Rafael <rdmattia@inf.ufpel.edu.br>
2024-04-06 17:56:23 -0300

    🚀 add routes for get attendee badge and get event

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ import {
 } from "fastify-type-provider-zod";
 import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
+import { getEvent } from "./routes/get-event";
+import { getAttendeBadge } from "./routes/get-attendee-badge";
 
 const app = fastify();
 
@@ -13,6 +15,8 @@ app.setSerializerCompiler(serializerCompiler);
 
 app.register(createEvent);
 app.register(registerForEvent);
+app.register(getEvent);
+app.register(getAttendeBadge);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP WServer Running! ");
