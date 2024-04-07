8b323f54735f5a46fcf4a7ba7baa763a2332f916
Rafael <rdmattia@inf.ufpel.edu.br>
2024-04-07 03:58:41 -0300

    🚀 add route to fetch all event attendees, paginated every 10 attendees, in descending order.

--- src/routes/get-attendee-badge.ts
+++ src/routes/get-attendee-badge.ts
@@ -3,7 +3,7 @@ import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod";
 import { prisma } from "../lib/prisma";
 
-export async function getAttendeBadge(app: FastifyInstance) {
+export async function getAttendeeBadge(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().get(
     "/attendees/:attendeeId/badge",
     {
--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,9 @@ import {
 import { createEvent } from "./routes/create-event";
 import { registerForEvent } from "./routes/register-for-event";
 import { getEvent } from "./routes/get-event";
-import { getAttendeBadge } from "./routes/get-attendee-badge";
+import { getAttendeeBadge } from "./routes/get-attendee-badge";
 import { checkIn } from "./routes/check-in";
+import { getEventAttendees } from "./routes/get-event-attendees";
 
 const app = fastify();
 
@@ -17,8 +18,9 @@ app.setSerializerCompiler(serializerCompiler);
 app.register(createEvent);
 app.register(registerForEvent);
 app.register(getEvent);
-app.register(getAttendeBadge);
+app.register(getAttendeeBadge);
 app.register(checkIn);
+app.register(getEventAttendees);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP WServer Running! ");
